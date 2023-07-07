0dd9a214bb5d8fb80ecefcc9cab449def31e5e6c
ahmederrami <ahmederrami@gmail.com>
2023-07-07 19:31:14 +0100

    rectif du fichier urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sitweb/', include('siteweb.urls')),
+    path('siteweb/', include('siteweb.urls')),
 ]
